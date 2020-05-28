c7a84ea2213ea5535c8222b67c3f84dd6c21d336
AlexMFV <alexmfv1@gmail.com>
2020-05-28 01:41:02 +0100

    Idk, not finished

---
+++ TimeTracker/TimeTracker/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace TimeTracker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.popup = new System.Windows.Forms.NotifyIcon(this.components);
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.lstProcesses = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // popup
+            // 
+            this.popup.Text = "notifyIcon1";
+            this.popup.Visible = true;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 480);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(181, 54);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTimer.Location = new System.Drawing.Point(521, 480);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(226, 57);
+            this.lblTimer.TabIndex = 1;
+            this.lblTimer.Text = "Timer";
+            this.lblTimer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lstProcesses
+            // 
+            this.lstProcesses.CheckBoxes = true;
+            this.lstProcesses.HideSelection = false;
+            this.lstProcesses.Location = new System.Drawing.Point(12, 12);
+            this.lstProcesses.Name = "lstProcesses";
+            this.lstProcesses.Size = new System.Drawing.Size(735, 465);
+            this.lstProcesses.TabIndex = 2;
+            this.lstProcesses.UseCompatibleStateImageBehavior = false;
+            this.lstProcesses.View = System.Windows.Forms.View.List;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(759, 546);
+            this.Controls.Add(this.lstProcesses);
+            this.Controls.Add(this.lblTimer);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon popup;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblTimer;
+        private System.Windows.Forms.ListView lstProcesses;
+    }
+}
+
---
+++ TimeTracker/TimeTracker/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimeTracker
+{
+    public partial class Form1 : Form
+    {
+        DateTime start_time;
+        Timer timer = new Timer();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Processes Logic
+            Process[] procs = Process.GetProcesses();
+
+            FillProcessesList(procs);
+
+            //Timer Logic
+            start_time = DateTime.Now;
+            btnStart.Click += BtnStart_Click;
+            timer.Interval = 200;
+            timer.Tick += Timer_Tick;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            lblTimer.Text = DateToTimeSpan(start_time);
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            start_time = DateTime.Now;
+            lblTimer.Text = DateToTimeSpan(start_time);
+            timer.Start();
+        }
+
+        string DateToTimeSpan(DateTime date1)
+        {
+            return DateTime.Now.Subtract(date1).ToString("h'h 'm'm 's's'");
+        }
+
+        void FillProcessesList(Process[] procs)
+        {
+            foreach(Process proc in procs)
+            {
+                ListViewItem item = new ListViewItem(proc.ProcessName);
+                lstProcesses.Items.Add(item);
+            }
+        }
+    }
+}
